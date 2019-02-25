fd2e182fb65511407230f1fd0ef9e0b76ccd6532
humbertokramm <humbertokramm@gmail.com>
2019-02-25 14:52:45 -0300

    - Ajuste para inverter o byte que veio ao contrário - Troca do db de imagem

--- teste.py
+++ teste.py
@@ -2,60 +2,35 @@ from PIL import Image
 import numpy as np
 import image_file as i
 
-# Monta o array da imagem a partir do formato RGB888_3valuesMatriz
-a = np.array(i.RGB888_3valuesMatriz, dtype=np.uint8)
-linhas = 60
+linhas = 20#60 #use 60 para a imagem completa
 colunas = 80
-
-for x in range (0,linhas):
-    for y in range (0,colunas):
-        for z in range (0,3):
-            if z == 0:
-                a[x,y,z] = a[x][y][z]<<3
-            elif z == 1:
-                a[x,y,z] = a[x][y][z]<<2
-            elif z == 2:
-                a[x,y,z] = a[x][y][z]<<3
+dataArray = i.RGB565_1
 
 # Monta o array da imagem a partir do formato RGB565             
 #Monta o RGB
-b=[0]*3
+a=[0]*3
 #Monta as Colunas
-b=[b]*colunas
+a=[a]*colunas
 #Monta as Linhas
-b=[b]*linhas
-b = np.array(b, dtype=np.uint8)
+a=[a]*linhas
+a = np.array(a, dtype=np.uint8)
 #Algorítimo montado baseado nesse site:
 #http://www.barth-dev.de/online/rgb565-color-picker/  
 for x in range (0,linhas):
     for y in range (0,colunas):
         index = y+colunas*x
-        R = i.RGB565[index]&0b1111100000000000
-        G = i.RGB565[index]&0b0000011111100000
-        B = i.RGB565[index]&0b0000000000011111
-        b[x,y,0] = R>>8
-        b[x,y,1] = G>>3
-        b[x,y,2] = B<<3
-
-#Verificação do algorítimo
-x = 3 #
-y = 5 #
-index = y+colunas*x
-print(format(bin(i.RGB565[index])))
-R = i.RGB565[index]&0b1111100000000000
-print("R ",format(bin(R)))
-G = i.RGB565[index]&0b0000011111100000
-print("G ",format(bin(G)))
-B = i.RGB565[index]&0b0000000000011111
-print("B ",format(bin(B)))
-b[x,y,0] = R>>8
-b[x,y,1] = G>>3
-b[x,y,2] = B<<3
-print(format(i.RGB565[index],'04X'))
-print(b[x][y])
+        pixel = dataArray[index]
+        #invert byte order
+        pixel = ((dataArray[index]&0xFF)<<8)|(dataArray[index]>>8)
+        #separa as cores
+        R = pixel&0b1111100000000000
+        G = pixel&0b0000011111100000
+        B = pixel&0b0000000000011111
+        #shift para a posi��o correta
+        a[x,y,0] = R>>8
+        a[x,y,1] = G>>3
+        a[x,y,2] = B<<3
 
 # Use PIL to create an image from the new array of pixels
 new_image = Image.fromarray(a,"RGB")
-new_image.save('a_new.png')
-new_image = Image.fromarray(b,"RGB")
-new_image.save('b_new.png')
+new_image.save('new.png')
